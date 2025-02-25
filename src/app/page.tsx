1cc00e0a654230c44472aadd04fa669a40ccffc8
Sayya Teja Swaroop <63931564+tejasayya@users.noreply.github.com>
2025-02-25 02:23:28 -0500

    snap4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function TestPage() {
           ))}
           <li>
             <a 
-              href="https://tejasayya.github.io/TejaaResume.pdf"
+              href="https://tejasayya.github.io/TejasResume.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="text-white hover:text-blue-400 transition-colors text-sm md:text-base"
